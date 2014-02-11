a3d902fa818ecd73be7dcab785e1fe66dcd44405
Michael Kimani <keymaney@gmail.com>
2014-02-11 03:24:46 +0300

    Add dashboard stats

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,24 @@ class StaticPagesController < ApplicationController
 
   def home
     if signed_in?
-  	    @scrape_sessions = current_user.scrape_sessions.limit(5) 
+        all_sessions = current_user.scrape_sessions
+
+  	    @scrape_sessions = all_sessions.limit(5)
+        
+        @has_more_sessions = true if all_sessions.count > 5
+
+        @all_pages_count    = 0
+        @all_posts_count    = 0
+        @all_comments_count = 0
+
+        all_pages = ScrapePage.where(scrape_session_id: all_sessions.select(:id))
+        @all_pages_count = all_pages.count
+
+        all_posts = FbPost.where(scrape_page_id: all_pages.select(:id))
+        @all_posts_count = all_posts.count
+
+        all_comments = FbComment.where(fb_post_id: all_posts.select(:id))
+        @all_comments_count = all_comments.count
     end 
   	
   end
