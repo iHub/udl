6e9891949d62f491b90dee0510a3d662640f1f6f
Michael Kimani <keymaney@gmail.com>
2014-02-13 14:07:53 +0300

    Activate runner method for cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,5 @@ set :output, "#{path}/log/cron.log"
 set :environment, :development
 
     every 10.minutes do
-      # runner "ScrapePage.new.collect_comments"
-      # runner "ScrapeSession.new.parse_all_sessions"
+      runner "ScrapeSession.new.parse_all_sessions"
     end
