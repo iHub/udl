4ffaa71659e9d974a9d03a1516514b31ec6b21e9
Michael Kimani <keymaney@gmail.com>
2014-02-17 15:39:57 +0300

    Remove unused test files

--- spec/views/init_scrape_logs/index.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "init_scrape_logs/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/new_page_logs/show.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "new_page_logs/show.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
