a080d8a7179ac5ea37fac0297422b4593e2f3aa4
Michael Kimani <keymaney@gmail.com>
2014-02-24 19:18:27 +0300

    Clean scrape pages controller

--- app/controllers/scrape_pages_controller.rb
+++ app/controllers/scrape_pages_controller.rb
@@ -4,18 +4,18 @@ class ScrapePagesController < ApplicationController
 	
 	def index
 		@scrape_session = get_scrape_session(params[:scrape_session_id])
-    	@scrape_pages = @scrape_session.scrape_pages
-    	@page_number = params[:page]
+    	@scrape_pages   = @scrape_session.scrape_pages
+    	@page_number    = params[:page]
 	end
 
 	def edit
 		@scrape_session = get_scrape_session(params[:scrape_session_id])
-    	@scrape_page = @scrape_session.scrape_pages.find(params[:id])
+    	@scrape_page    = @scrape_session.scrape_pages.find(params[:id])
 	end
 
 	def new
 		@scrape_session = get_scrape_session(params[:scrape_session_id])
-    	@scrape_page = @scrape_session.scrape_pages.build
+    	@scrape_page    = @scrape_session.scrape_pages.new
     	@provide_default_instructions = true
 	end
 
@@ -40,7 +40,7 @@ class ScrapePagesController < ApplicationController
 
 	def update
 		@scrape_session = get_scrape_session(params[:scrape_session_id])
-		@scrape_page = ScrapePage.find(params[:id])
+		@scrape_page    = ScrapePage.find(params[:id])
 		@scrape_page.continous_scrape = params[:scrape_page][:continous_scrape]
 		@scrape_page.override_session_settings  = params[:scrape_page][:override_session_settings]
 		@scrape_page.scrape_frequency = frequency_minutes params[:scrape_page][:scrape_frequency_select]
@@ -50,25 +50,20 @@ class ScrapePagesController < ApplicationController
 			flash[:success] = "Your Page has been updated."
 			redirect_to scrape_session_scrape_page_path
 		else
-			render 'edit'
+			render :edit
 		end
 	end
 
 	def show
 		@scrape_session = get_scrape_session(params[:scrape_session_id])
-		
-		@scrape_page = @scrape_session.scrape_pages.find(params[:id])
-
-		@fb_posts = @scrape_page.fb_posts.paginate(page: params[:page]).includes(:scrape_page)
-
-		@page_number = params[:page]
-
+		@scrape_page    = @scrape_session.scrape_pages.find(params[:id])
+		@fb_posts       = @scrape_page.fb_posts.paginate(page: params[:page]).includes(:scrape_page)
+		@page_number    = params[:page]
 		@selected_posts = {}
 		@selected_posts[:source] = "Page"
 
 		@search = FbPost.search(params[:q])
     	@search.build_condition
-    	
 	end
 
 	def destroy
