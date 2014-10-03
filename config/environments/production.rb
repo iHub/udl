2d1470ea32e1dc5c93948efd5047be18880026df
Charlie Chuck <charles.ongao@zegetech.com>
2014-10-03 12:39:11 +0300

    fixed typo in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,13 +13,13 @@ ULog::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  onfig.cache_store = :dalli_store  
+  config.cache_store = :dalli_store  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
   config.assets.compress = true
-  
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
