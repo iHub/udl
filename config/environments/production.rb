1b1d658d0a1cdead721fbf226a1cb378bca5a75d
Charlie Chuck <charles.ongao@zegetech.com>
2014-10-03 12:45:32 +0300

    removed config.cache_store = :dalli_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ ULog::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.cache_store = :dalli_store  
+  # config.cache_store = :dalli_store  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
