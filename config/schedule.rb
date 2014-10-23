293365d1c0db26201488a0f408c527e0629628af
Charlie Chuck <charles.ongao@zegetech.com>
2014-10-23 11:09:19 +0300

    updated my schedule.rb for whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,7 +10,7 @@ set :environment, :production
 	# 	rake "disqus:fetch"
 	# end
 
-  every 10.minutes do
-    # runner "ScrapePage.new.collect_comments"
-    # runner "ScrapeSession.new.parse_all_sessions"
-  end
+  # every 10.minutes do
+  #   # runner "ScrapePage.new.collect_comments"
+  #   # runner "ScrapeSession.new.parse_all_sessions"
+  # end
